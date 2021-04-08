e4bfed3ef9be99469e1a7d7b30a69d2bbae213f9
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2021-04-09 00:18:15 +0200

    substract ryzen offset

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ sio.connect('http://localhost:4444')
 # sio.emit('cpuUsage', {'id': str(datetime.now()), 'type': 'USAGE', 'value': psutil.cpu_percent()})
 # print(nvidia_temperature())
 while True:
-    sio.emit('cpu', {'id': str(datetime.now()), 'usage': psutil.cpu_percent(), 'temp': psutil.sensors_temperatures().get('k10temp')[0].current})  # ryzxen 2700x
+    sio.emit('cpu', {'id': str(datetime.now()), 'usage': psutil.cpu_percent(), 'temp': psutil.sensors_temperatures().get('k10temp')[0].current - 10})  # ryzxen 2700x minus offset
     sio.emit('memory', {'id': str(datetime.now()), 'usage': psutil.virtual_memory().percent})
     sio.emit('gpu', {'id': str(datetime.now()), 'usage': nvidia_utilization().get('Gpu')[:-2], 'temp': nvidia_temperature().get('GPU Current Temp')[:-2], 'memory': nvidia_utilization().get('Memory')[:-2]})
     time.sleep(1)
